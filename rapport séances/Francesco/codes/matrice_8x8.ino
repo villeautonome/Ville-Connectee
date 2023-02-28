5adba2f3be3ab0e175aa3d1fe7af06e2fa3557a3
francescors <120786934+francescors@users.noreply.github.com>
2023-02-28 20:39:34 +0100

    Update matrice_8x8.ino

--- rapport séances/Francesco/codes/matrice_8x8.ino
+++ rapport séances/Francesco/codes/matrice_8x8.ino
@@ -1,3 +1,22 @@
+Le code fourni est un programme pour contrôler une matrice LED 8x8 à l'aide du circuit intégré MAX7219.
+Les variables suivantes sont utilisées pour définir les broches utilisées pour la communication avec la matrice LED :
+* DIN_pin : le numéro de la broche connectée au pin DIN de la matrice LED, qui sert à envoyer les données.
+* CS_pin : le numéro de la broche connectée au pin CS de la matrice LED, qui est utilisé pour activer la communication avec la matrice LED.
+* CLK_pin : le numéro de la broche connectée au pin CLK de la matrice LED, qui est utilisé pour synchroniser l'envoi des données.
+Les variables suivantes sont utilisées pour stocker les motifs à afficher sur la matrice LED :
+* C1, C2, C3, C4 : ces variables sont des tableaux d'octets qui définissent les motifs à afficher sur la matrice LED pour chaque chiffre (1 à 4).
+La fonction write_pix est utilisée pour envoyer un octet de données à la matrice LED en utilisant la liaison série. Elle prend un paramètre data qui est l'octet de données à envoyer. La fonction envoie les bits un par un, en commençant par le bit le plus significatif (MSB).
+La fonction write_line est utilisée pour écrire une ligne de données à une adresse spécifique sur la matrice LED. Elle prend deux paramètres : l'adresse de la ligne à écrire (adress) et la ligne de données (data). La fonction utilise write_pix pour envoyer les octets de données appropriés à la matrice LED.
+La fonction write_matrix est utilisée pour écrire une matrice de données à la matrice LED. Elle prend un tableau de 8 octets (tab) qui représente les données de la matrice à afficher. La fonction utilise write_line pour écrire chaque ligne de données à la matrice LED.
+La fonction init_MAX7219 est utilisée pour initialiser le circuit MAX7219. Elle écrit plusieurs paramètres de configuration à la matrice LED en utilisant write_line. Ces paramètres sont les suivants :
+* 0x09 : le paramètre de décodage BCD, qui est défini sur 0 pour désactiver le décodage BCD.
+* 0x0A : la luminosité de la matrice LED, qui est définie sur 1 (la valeur maximale).
+* 0x0B : la limite de numérisation, qui est définie sur 8 LED (pour une matrice LED 8x8).
+* 0x0C : le mode d'alimentation, qui est défini sur 1 pour activer le mode normal de la matrice LED.
+* 0x0F : le registre d'affichage test, qui est défini sur 0 pour désactiver le mode de test.
+La fonction clear_matrix est utilisée pour effacer la matrice LED. Elle utilise write_matrix pour écrire une matrice vide sur la matrice LED.
+"La fonction intToHex est utilisée pour convertir un entier en un octet qui représente l'état d'une LED sur la matrice LED. Elle prend un entier x qui représente l'index de la LED (de 0 à 7) et renvoie un octet avec un seul bit défini à l'index correspondant, indiquant que la LED doit être allumée."
+
 int DIN_pin = 11; // déclaration de la broche de données
 int CS_pin = 10; // déclaration de la broche de sélection de circuit
 int CLK_pin = 12; // déclaration de la broche d'horloge
